453a9c80470c3bcaaa11b89adf1bfbda8b2fb79d
Hafsa <hahmed20@uoguelph.ca>
2025-10-20 13:20:10 -0400

    word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -8,6 +8,7 @@
 # 4. Iterate through words and update frequencies
 
 import re
+import string
 
 #This is a function that checks if a text qualifies as a sentence. You do not need to modify this!
 def is_sentence(text):
@@ -29,9 +30,37 @@ def is_sentence(text):
 
     return True
 
+# 1. Prompt the user
 user_sentence = input("Enter a sentence: ")
-
 while (is_sentence(user_sentence) == False):
     print("This does not meet the criteria for a sentence.")
     user_input = input("Enter a sentence: ")
+
+# 2. Split the sentence into words
+updated_sentence = user_sentence #removing punctuation from being counted as a word
+for p in string.punctuation:
+    updated_sentence = updated_sentence.replace(p, '')
+
+updated_sentence = updated_sentence.lower() #Converting to lowercase so words with diff capitalizations are treated as the same word
+
+words_list = updated_sentence.split() #Split the input sentence into a list of words using the split() method
+
+# 3. Create lists
+words = []
+frequencies = []
+
+# 4. Iterate through words and update frequencies
+for word in words_list: #Iterate through the list of words
+    if word in words: #Check for existence
+        index = words.index(word)
+        frequencies[index] += 1 #Update frequency: If the word exists, update its frequency in the corresponding list
+    else:
+        words.append(word) #Otherwise, append both the word and a frequency of 1 to the lists.
+        frequencies.append(1)
+    
+# 5. Print results
+for i in range(len(words)):
+    print(f"{words[i]}: {frequencies[i]}")
+
+
     
