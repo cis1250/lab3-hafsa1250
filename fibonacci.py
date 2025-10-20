1874f6107a367fa70d753061c4dbcff2e4880353
Hafsa <hahmed20@uoguelph.ca>
2025-10-20 11:23:38 -0400

    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -7,12 +7,12 @@
 
 #Validating that the input is a positive integer.
 while True:
-      user_input = int(input("How many terms do you want?")) # Prompt the user for the number of terms.
+    user_input = input("How many terms do you want?") # Prompt the user for the number of terms.
 
-  if user_input.isdigit(): #checking if input is a positive integer
-       number_of_terms = int(user_input) #Converts the number typed as text into an actual number
-       break #If it's a valid input, then exit the loop
-else:
+    if user_input.isdigit():
+        number_of_terms = int(user_input)
+        break #If it's a valid input, then exit the loop
+    else:
         print("Please enter a positive integer") #If the input is invalid, display an error message and prompt again
 
 # Use a for loop to print the Fibonacci sequence up to that many terms.
@@ -20,7 +20,8 @@ a = 0
 b = 1 
 
 for i in range (number_of_terms):
-  print(a, end=" ") #prints the current value of a
-  a, b = b, a + b #Update a and b for the next term
-  
-# Your code doesn't run, you have an indentation problem -1
+  print(a, end=" ")
+  temp = a #prints the current value of a
+  a = b
+  b =  temp + b
+      #Update a and b
